7e565f3e85e7830ce9d68b36bc84eb8b641463b4
Blanche (angelroom01) <174630752+blnchdev@users.noreply.github.com>
2026-04-28 03:02:47 +0200

    feat(Elo): !elo command

---
+++ Components/Commands/Elo/Elo.cpp
@@ -0,0 +1,20 @@
+#include "Elo.h"
+
+#include "Globals/Globals.h"
+
+namespace Components::Operation
+{
+	void Elo( const Command* Data )
+	{
+		const auto Response = Globals::LeagueAPI->GetLeagueRank( Data->ChannelID );
+
+		if ( Response.has_value() )
+		{
+			Globals::Bot->ReplyTo( *Data->Context, Response.value() );
+		}
+		else
+		{
+			Globals::Bot->ReplyTo( *Data->Context, std::format( "{} is currently not ranked", Data->ChannelID ) );
+		}
+	}
+}
